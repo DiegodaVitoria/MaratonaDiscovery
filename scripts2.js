6f3f34d6f87dae86e9931cd730cfa0463289d27f
Diego Carlos Reis da Vitoria <53801283+DiegodaVitoria@users.noreply.github.com>
2021-02-12 10:35:29 -0300

    login autentic

--- scripts2.js
+++ scripts2.js
@@ -19,7 +19,18 @@ const Modal = {
 }
 
 
-
+function Login() {
+    var done=0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario=usuario.toLowerCase();
+    var senha= document.getElementsByName('senha')[0].value;
+    seha=senha.toLowerCase();
+    if (usuario=="admin@admin" && senha=="admin") {
+      window.location="/index2.html";
+      done=1;
+    }
+    if (done==0) { alert("Dados incorretos, tente novamente"); }
+  }
 
 
 
